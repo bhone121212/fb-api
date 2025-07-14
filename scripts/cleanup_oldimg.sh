1187656b9ca76daad9ce8ea04dc6687d3dc8360e
bhone121212 <bhonenaung777@gmail.com>
2025-07-14 15:03:28 +0630

    678

--- scripts/cleanup_oldimg.sh
+++ scripts/cleanup_oldimg.sh
@@ -8,7 +8,7 @@ KEEP_COUNT=5
 echo "🧹 Cleaning up old images for $IMAGE_NAME, keeping only the latest $KEEP_COUNT"
 
 buildah images --json | jq -r '
-  .[] | select(.Name == "'"$IMAGE_NAME"'") | "\(.CreatedAt) \(.Id)"
+  .[] | select(.Repository == "'"$IMAGE_NAME"'") | "\(.CreatedAt) \(.Id)"
 ' | sort -r | awk '{print $2}' > all_ids.txt
 
 head -n $KEEP_COUNT all_ids.txt > keep_ids.txt
